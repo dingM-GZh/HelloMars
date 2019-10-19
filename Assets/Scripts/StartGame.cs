16e099cb287698861c22291b078ef843d2654cbf
Maxwell Ding <mbding@gmail.com>
2019-10-19 12:05:52 -0500

    title menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private SceneLoader scene_loader;
+
+    private void Start()
+    {
+        scene_loader = FindObjectOfType<SceneLoader>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        //throw new NotImplementedException();
+        /* int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);*/
+        
+        scene_loader.LoadNextScene();
+    }
+}
---
+++ Assets/Scripts/TextFadeInTMP.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextFadeInTMP : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<TextMeshProUGUI>()));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public IEnumerator FadeTextToFullAlpha(float t, TextMeshProUGUI i) {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+    
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
