09ae6e0b2234287a6bfed0f6dae7d7013d3a2a0d
Maxwell Ding <mbding@gmail.com>
2019-10-19 18:22:38 -0500

    fixing components

--- Assets/Scripts/AfterTitle.cs
+++ Assets/Scripts/AfterTitle.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 public class AfterTitle : MonoBehaviour
 {
     private SceneLoader scene_loader;
-    private bool is_loaded = false;
 
     private void Start()
     {
         scene_loader = FindObjectOfType<SceneLoader>();
-        Debug.Log("FOUND SCENE LOADER");
     }
-
-    private void Update()
-    {
-        //throw new NotImplementedException();
-        
-    }
-
+    
     private void OnTriggerEnter(Collider collider)
     {
         //throw new NotImplementedException();
         /* int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);*/
-        //Debug.Log("TRIGGER");
-        if (!is_loaded && collider.gameObject.CompareTag("Glasses") )
-        {
-            Debug.Log("CHANGE SCENE");
-            scene_loader.LoadNextScene();
-            is_loaded = true;
-        }
-        else
-        {
-            Debug.Log("ALREADY LOADED");
-        }
-        
+        scene_loader.LoadNextScene();
     }
 }
