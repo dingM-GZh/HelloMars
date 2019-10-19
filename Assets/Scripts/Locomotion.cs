0688feeab1d6e8978225243cac1ae4e6e4edf308
Maxwell Ding <mbding@gmail.com>
2019-10-19 10:16:46 -0500

    first commit, using Lightweight VR Template

---
+++ Assets/Scripts/Locomotion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locomotion : MonoBehaviour {
+
+    [SerializeField]
+    private float  maxSpeed = 2;
+    [SerializeField]
+    private float acceleration = 1;
+    
+    private Vector3 velocity = Vector3.zero;
+    
+    private void Update() {
+
+        Vector3 newVel = Vector3.Lerp(velocity, Vector3.zero, acceleration);
+
+        float x = VRInput.GetAxis(VRButton.LeftThumbHorizontal);
+        float z = VRInput.GetAxis(VRButton.LeftThumbVertical);
+
+        print("X: " + x);
+        print("Z: " + z);
+        
+        newVel.x += x;
+        newVel.z += z;
+
+        velocity = Vector3.ClampMagnitude(newVel, maxSpeed);
+        transform.Translate(velocity * Time.deltaTime);
+
+    }
+}
